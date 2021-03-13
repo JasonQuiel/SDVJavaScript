6ef6f09f3fb165c82e45c505b14d3212628bb131
Jason <Jason-Quiel@live.nmit.ac.nz>
2021-03-13 14:02:12 +1300

    weekthree.js updated

--- src/Weekthree.js
+++ src/Weekthree.js
@@ -1,7 +1,20 @@
 //Basic of JavaScript
 // Var - Let - Const
 
-console.log('Hello World')
-console.log('Welcome')
-console.log('Check')
-//Prints to console 
+//Let variable NMIT Address
+
+let letAddress = ('322 Hardy Street')
+
+let letCity = ('Nelson')
+
+//let variable Country, New Zealand
+
+let letCountry = ('New Zealand')
+
+//Let variable Zip, 7010
+
+let letZip = ('7010')
+
+let fullAddress = letAddress+'  '+letCity+'  '+letCountry
+//print 
+console.log(fullAddress)
