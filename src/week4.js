2cf447cb80619f3c4e7b164e6e67696f72a3ca4a
Jason <Jason-Quiel@live.nmit.ac.nz>
2021-03-15 12:49:29 +1300

    Week4.js Updated

---
+++ src/week4.js
@@ -0,0 +1,21 @@
+
+//Values and variables
+//Review
+
+
+let country = "New Zealand"
+//declare a variable (let), named that variable (country), assigned a string ("New Zealand")
+let city = "Nelson"
+//declare a variable (let), name that variable (city), assigned a string ("Nelson")
+let postalCode = 7010
+
+//Prints information to console
+console.log(typeof country)  //typeof shows the data type (boolean, string)
+console.log(city)
+console.log(postalCode)
+
+let isIsland = true
+let language  //undefined Variable
+
+console.log(isIsland) //typeof isIsland is a boolean
+console.log(language)
