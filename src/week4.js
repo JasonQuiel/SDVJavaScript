ab8bbded26ce77e2def429325856bb95c98b82cb
Jason <Jason-Quiel@live.nmit.ac.nz>
2021-03-15 13:26:37 +1300

    Week4.js Updated

--- src/week4.js
+++ src/week4.js
@@ -2,15 +2,15 @@
 //Values and variables
 //Review
 
-
 let country = "New Zealand"
 //declare a variable (let), named that variable (country), assigned a string ("New Zealand")
 let city = "Nelson"
 //declare a variable (let), name that variable (city), assigned a string ("Nelson")
 let postalCode = 7010
+//let (variable) postalCode (name of variable) 7010 
 
 //Prints information to console
-console.log(typeof country)  //typeof shows the data type (boolean, string)
+console.log(typeof country)  //typeof shows the data type (string)
 console.log(city)
 console.log(postalCode)
 
@@ -19,3 +19,21 @@ let language  //undefined Variable
 
 console.log(isIsland) //typeof isIsland is a boolean
 console.log(language)
+
+language = "Arabic"  //Now a defined variable from line 17
+console.log(language)
+
+let number = 10
+console.log(number)
+
+console.log(number /2)
+console.log(number > 6)
+console.log(number < 2)
+
+const address = country + " " + city + " " + postalCode  //Three variables combined
+console.log(address)
+
+let string = `My address is ${country}, ${city}, ${postalCode}.` //String template. Keep in mind ` `
+console.log(string)
+
+
