bf195b912bd19b843b4d79029570a31cc5d93e35
Jason <Jason-Quiel@live.nmit.ac.nz>
2021-03-15 14:37:13 +1300

    Week4.js Updated

--- src/week4.js
+++ src/week4.js
@@ -7,7 +7,7 @@ let country = "New Zealand"
 let city = "Nelson"
 //declare a variable (let), name that variable (city), assigned a string ("Nelson")
 let postalCode = 7010
-//let (variable) postalCode (name of variable) 7010 
+//let (variable) postalCode (name of variable) 7010 (assigned as a number)
 
 //Prints information to console
 console.log(typeof country)  //typeof shows the data type (string)
