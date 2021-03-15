def988f8935b3942049765319a3c0b4a604e4f15
Jason <Jason-Quiel@live.nmit.ac.nz>
2021-03-16 10:03:40 +1300

    weds.js updated

---
+++ src/weds.js
@@ -0,0 +1,26 @@
+x = '12345678';
+console.log(typeof x)
+let y = Number (x)
+//console.log(typeof y);
+
+const a = 1+1
+console.log(a)
+
+const b = a + 2
+console.log(b)
+
+let redLion
+console.log(redLion)
+
+const information = {  //Ojbect = student value = Jason
+    student: 'Jason', //String
+    country: 'New Zealand',
+    city: 'Nelson',
+    postalCode: 7010, //Number
+}
+
+const travel = {
+    car: 'Mazda',
+    model: 'Demio',
+    year: '2010',    
+}
