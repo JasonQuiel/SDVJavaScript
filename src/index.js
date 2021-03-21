d848a89a8800bf505f20fcd473fbba5929372d10
Jason <Jason-Quiel@live.nmit.ac.nz>
2021-03-22 11:50:04 +1300

    changes

--- src/index.js
+++ src/index.js
@@ -1,29 +1,4 @@
-//TooDo list
 
-let tempCelsius = 6
-let tempFahrenheit = 2
-let tempKelvin = 9
-
-console.log('convert c to K')
-
-if (tempCelsius > tempKelvin) {
-    console.log('this false')
-}
-else {
-let resultC = tempCelsius  + 273 
-console.log(resultC + 'K')  
-}
-
-console.log('convert f to k')
-
-if (tempFahrenheit > tempKelvin) {
-    console.log('this false')
-}
-else {
-    let resultFK = tempFahrenheit - 32 *5 /9 +273
-    console.log(resultFK + "k")
-}
- 
 let x = 5
 let y = 6
 let z = 7
@@ -37,4 +12,4 @@ if (x === a) {
 } else if (z === a) {
     console.log(z)
 }
-    
+
