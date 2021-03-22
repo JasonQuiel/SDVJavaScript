ba6a60c2291528c6fe11ba3a4331338760d3310c
Jason <Jason-Quiel@live.nmit.ac.nz>
2021-03-22 14:13:28 +1300

    stuff

--- src/weekfive.js
+++ src/weekfive.js
@@ -57,3 +57,60 @@ switch (foo) {
         console.log('I don\'t like this number')
 }
 
+const goo = 1;
+let output = '';
+switch (goo) {
+  case 0:
+    output += 'So ';
+  case 1:
+    output += 'How ';
+    output += 'Old ';
+  case 2:
+    output += 'Are ';
+  case 3:
+    output += 'You';
+  case 4:
+    output += '?';
+    console.log(output);
+    break; //Keeps going until it hits this break.
+  case 5:
+    output += '!';
+    console.log(output);
+    break;
+  default:
+    console.log('Please pick a number from 0 to 5!');
+}
+
+let applicant = {
+    name: 'tony',
+        age: 17,
+            driver: null
+}
+if (applicant.age >=16){
+        applicant.driver = 'Yes'
+} else {
+    applicant.driver = 'no'
+}
+console.log(applicant.driver)
+
+applicant.driver = applicant.age >=16 ? 'Yes' : 'No'
+console.log(applicant)
+
+let x = 5
+let y = 5 <= x ? 5 + 5 : x
+console.log(y)
+
+let isStudent = 8
+let isGeneral = 12
+switch (isStudent) {
+    case 12:
+       console.log('General Admission')
+            break
+    case 8:
+        console.log('Student Admission')
+            break
+}
+
+
+let price = isStudent ? `price is 8` : `price is 12` 
+console.log(price)
